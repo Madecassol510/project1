5c04bc1629c66a01619bc6c26c14c5deb48d4b79
Yu tae young <yut0613@naver.com>
2023-09-12 13:57:47 +0900

    태용 테스트입니다

--- TeamProject/src/project/Test01.java
+++ TeamProject/src/project/Test01.java
@@ -2,6 +2,6 @@ package project;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.println("테스트입니다");
+		System.out.println("태용 테스트입니다");
 	}
 }
---
+++ TeamProject/src/project/Test2.java
@@ -0,0 +1,7 @@
+package project;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("테스트입니다");
+	}
+}
